025e84908707de55b12b6ddb8c113d7ccbd69e26
Tao Feng <71760715+s2014628@users.noreply.github.com>
2022-07-29 17:54:22 +0100

    a

--- aa.py
+++ aa.py
@@ -1,17 +1,10 @@
 from multiprocessing.sharedctypes import Value
 from selenium import webdriver
-import uuid
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import os
-import shutil
-import urllib.request
-import json
-import requests
-import selenium
 from selenium.webdriver import Chrome
 !pip install webdriver-manager
 from webdriver_manager.chrome import ChromeDriverManager
@@ -33,6 +26,38 @@ class scraper:
     def accept_cookies(self):
         cookies_button=self.driver.find_element(by=By.XPATH,value='//*[@id="onetrust-accept-btn-handler"]')
         cookies_button.click()
+        time.sleep(2)
+        return self.driver
+    
+
+    def get_product_list(self):
+        product_container=self.driver.find_element(by=By.XPATH,value='//*[@id="main-content"]/div[2]/div[2]/ul')
+        product_list=product_container.find_elements(by=By.XPATH,value='./li')
+        return product_list
+    
+    def get_product_name(self):
+        tag_product_name=self.driver.find_element(by=By.CLASS_NAME ,value="bop-title")
+        name=tag_product_name.find_element(by=By.XPATH,value='.//h1').text
+        return name
+    
+    def get_product_weight(self):
+        tag_product_weight=self.driver.find_element(by=By.CLASS_NAME ,value="bop-title")
+        weight=tag_product_weight.find_element(by=By.XPATH,value='.//span[@class="bop-catchWeight"]').text
+        return weight
+    def get_product_price(self):
+        get_prduct_price=self.driver.find_element(by=By.XPATH,value='//h2[@class="bop-price__current " ]').text
+        return get_prduct_price
+    
+
+
+
+
+
+
+
+    
+product_list=product_container.find_elements(by=By.XPATH,value='./li')
+num_list=len(product_list)
 
 
 
